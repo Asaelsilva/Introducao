9b62a7838a05c08de97f37815e0b60e5d3f1560f
asael joao <asael.guitar_g3@hotmail.com>
2019-08-14 14:13:14 -0300

    Adicionar arquivos de projeto.

---
+++ Introducao/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Introducao.Models;
+using System.Web.Mvc;
+
+namespace Introducao.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            var pessoa = new Pessoa()
+            {
+                PessoaId = 1,
+                Name = "Asael João",
+                Tipo = "Administrador"
+            };
+
+            //Usando ViewData
+            //ViewData["PessoaId"] = pessoa.PessoaId;
+            //ViewData["Name"] = pessoa.Name;
+            //ViewData["Tipo"] = pessoa.Tipo;
+
+            //Usando viewbag
+            //ViewBag.Id = pessoa.PessoaId;
+            //ViewBag.Name = pessoa.Name;
+            //ViewBag.Tipo = pessoa.Tipo;
+
+
+            return View(pessoa);
+        }
+        public ActionResult Contatos()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Lista()
+        {
+            return View();
+        }
+    }
+}
